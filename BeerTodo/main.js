1ba79ff3331800ca72741c72bacbcb6bfa1552fa
Bunreal <bunreal0@gmail.com>
2022-05-16 00:58:18 +0800

    up

--- BeerTodo/main.js
+++ BeerTodo/main.js
@@ -1,7 +1,20 @@
+const xxx = document.querySelector('#labeltest')
+xxx.addEventListener('click', ()=>{
+  console.log('3')
+})
+
 new Vue({
   el: "#app",
   data: {
     title: 'bear todo',
-    newOne: ''
+    newOne: '',
+    test: {
+      id: uuidv4(),
+    }
+  },
+  methods: {
+    test2(){
+      console.log('2')
+    }
   }
 });
